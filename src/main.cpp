894ec1e86e69883bf668fa06a0c5b0edbbe52126
Saurav_Gautam_46 <sauravgautam452339@gmail.com>
2026-02-11 09:38:12 +0530

    Write some lines of code in setup function

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,19 @@
 #define OLED_ADDR 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_ADDR);
 void setup() {
+    Serial.begin(9600);
+    if (!display.begin(SSD1306_SWITCHCAPVCC,OLED_ADDR))
+    {
+        Serial.println("OLED not found");
+        while (true);
+    }
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0,0);
+    display.println("Arduino Uno R4");
+    display.println("Hello Student");
+    display.display();
 // write your initialization code here
 }
 
